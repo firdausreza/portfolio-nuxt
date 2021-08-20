bb70aa45d5dcf43ae99fc044fbff81456af8adb5
Reza Azerly <kiritosao080@gmail.com>
2021-08-20 16:43:53 +0700

    possibly last commit

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <header class="sticky top-0 z-50 ">
       <nav class="w-full flex flex-col justify-center items-center mx-auto py-8 bg-dblue">
         <h1 class="text-5xl font-bold">
-          Reza Firdaus
+          Welcome!
         </h1>
         <div class="flex flex-row justify-center items-center mt-8">
           <nuxt-link :to="{ path: '/', hash:'#about'}" class="hover:text-lred">
